f5c7672d8ede35b0614256fbc1a6fec29eb4c406
sha <tylkonieto1@gmail.com>
2013-03-24 17:13:44 +0100

    visual effects for displaying photos, added some jQuery code to handle buttons

--- js/main.js
+++ js/main.js
@@ -10,4 +10,41 @@ $(document).ready(function() {
 		counterText: 'Pozostało: '
 	});
 
+
+	/*  
+		trochę kodu dla wizualizacji kliknięcia danego zdjęcia + dodanie
+		eventów dla przycisków tak być działały 
+	*/
+	if (($(".etap-drugi ul.thumbnails").children().length) > 1) { 
+		
+		console.log("zdjęcia w kolekcji, eventy ON");
+
+		$(".etap-drugi ul li a").on('click', function(event) {
+			event.preventDefault();
+			$(this).toggleClass("selected");
+			console.log("img clicked");
+		});
+
+		$("#odznacz-wszystkie").on('click', function(event) {
+			event.preventDefault();
+			$(".etap-drugi ul li").each(function() {
+				$(this).find('a').removeClass("selected");
+			});
+		});
+
+		$("#zaznacz-wszystkie").on('click', function(event) {
+			event.preventDefault();
+			$(".etap-drugi ul li").each(function() {
+				$(this).find('a').addClass("selected");
+			});
+		});
+
+		// przyciski mogą być ukryte, let's show them
+		$("#odznacz-wszystkie").show();
+		$("#zaznacz-wszystkie").show(); 
+	} else {
+		$(".etap-drugi ul.thumbnails").append('<h2>Brak dodanych zdjęć</h2><p class="lead"> Nie zostały dodane jeszcze żadne zdjęcia do kolekcji, dodaj coś żeby móc stworzyć kampanię.</p>');
+		$("#odznacz-wszystkie").hide();
+		$("#zaznacz-wszystkie").hide();
+	}
 });
