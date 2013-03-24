2ceea34a148bb0b805570dad115be79db10f3bda
sha <tylkonieto1@gmail.com>
2013-03-24 21:10:47 +0100

    added buttons to section 3 - choosing accounts, some modification with jQuery

--- js/main.js
+++ js/main.js
@@ -25,14 +25,14 @@ $(document).ready(function() {
 			console.log("img clicked");
 		});
 
-		$("#odznacz-wszystkie").on('click', function(event) {
+		$("#odznacz-wszystkie-zdjecia").on('click', function(event) {
 			event.preventDefault();
 			$(".etap-drugi ul li").each(function() {
 				$(this).find('a').removeClass("selected");
 			});
 		});
 
-		$("#zaznacz-wszystkie").on('click', function(event) {
+		$("#zaznacz-wszystkie-zdjecia").on('click', function(event) {
 			event.preventDefault();
 			$(".etap-drugi ul li").each(function() {
 				$(this).find('a').addClass("selected");
@@ -40,12 +40,12 @@ $(document).ready(function() {
 		});
 
 		// przyciski mogą być ukryte, let's show them
-		$("#odznacz-wszystkie").show();
-		$("#zaznacz-wszystkie").show(); 
+		$("#odznacz-wszystkie-zdjecia").show();
+		$("#zaznacz-wszystkie-zdjecia").show(); 
 	} else {
 		$(".etap-drugi ul.thumbnails").append('<h2>Brak dodanych zdjęć</h2><p class="lead"> Nie zostały dodane jeszcze żadne zdjęcia do kolekcji, dodaj coś żeby móc stworzyć kampanię.</p>');
-		$("#odznacz-wszystkie").hide();
-		$("#zaznacz-wszystkie").hide();
+		$("#odznacz-wszystkie-zdjecia").hide();
+		$("#zaznacz-wszystkie-zdjecia").hide();
 	}
 
 	/* zajmujemy się kontami, podobne efekty & kod jak dla zdjęć */
@@ -57,8 +57,28 @@ $(document).ready(function() {
 			$(this).toggleClass("selected");
 			console.log("konto clicked");
 		});
+
+		$("#odznacz-wszystkie-konta").on('click', function(event) {
+			event.preventDefault();
+			$(".etap-trzeci ul li").each(function() {
+				$(this).removeClass("selected");
+			});
+		});
+
+		$("#zaznacz-wszystkie-konta").on('click', function(event) {
+			event.preventDefault();
+			$(".etap-trzeci ul li").each(function() {
+				$(this).addClass("selected");
+			});
+		});
+		// przyciski mogą być ukryte, let's show them
+		$("#odznacz-wszystkie-konta").show();
+		$("#zaznacz-wszystkie-konta").show();
 	} else {
 		$(".etap-trzeci ul.media-list").append('<h2>Brak dodanych kont</h2><p class="lead">Nie zostały dodane jeszcze żadne konta, dodaj najpierw konto by móc stworzyć kampanię</p>');
+		// przyciski mogą być ukryte, let's show them
+		$("#odznacz-wszystkie-konta").hide();
+		$("#zaznacz-wszystkie-konta").hide();
 	}
 
 });
